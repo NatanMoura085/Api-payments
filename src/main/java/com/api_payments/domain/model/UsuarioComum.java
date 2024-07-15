e54044c3033ed9399af492c49e70dfb47622f10d
Natan Moura <mouranatan933@gmail.com>
2024-07-15 14:13:42 -0300

    feature: create entity usuarioComun

---
+++ src/main/java/com/api_payments/domain/model/UsuarioComum.java
@@ -0,0 +1,29 @@
+package com.api_payments.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+@Entity
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "usuario_comum")
+public class UsuarioComum {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    @NotNull
+    private String nomeCompleto;
+    @Max(11)
+    private String cpf;
+    private String email;
+    @Max(60)
+    private String senha;
+    @Column(name = "saldo_da_conta")
+    private BigDecimal saldoConta;
+
+}
