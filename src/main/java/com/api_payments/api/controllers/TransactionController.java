a8d865b6ab9c91e705a232672ff1cfc0f6454ee2
Natan Moura <mouranatan933@gmail.com>
2024-07-26 16:14:13 -0300

    feature:loading..

--- src/main/java/com/api_payments/api/controllers/TransactionController.java
+++ src/main/java/com/api_payments/api/controllers/TransactionController.java
@@ -4,10 +4,7 @@ import com.api_payments.domain.model.Transaction;
 import com.api_payments.domain.service.TransactionService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -17,5 +14,9 @@ public class TransactionController {
     @GetMapping("/transaçoes/{id}")
     public ResponseEntity<Transaction> listaDeTransaçoes(@PathVariable Long id){
         return transactionService.todasTrasaçoes(id);
+    }
+    @PostMapping("/transaçoes")
+    public Transaction cadastrarTrnsaçao(@RequestBody Transaction transaction){
+        return transactionService.fazerTransaçao(transaction);
     }
 }
--- src/main/java/com/api_payments/domain/service/TransactionService.java
+++ src/main/java/com/api_payments/domain/service/TransactionService.java
@@ -1,20 +1,42 @@
 package com.api_payments.domain.service;
 
 import com.api_payments.domain.exceptionhandler.TransactionException;
+import com.api_payments.domain.model.StatusTransaction;
 import com.api_payments.domain.model.Transaction;
+import com.api_payments.domain.model.UsuarioLojista;
 import com.api_payments.domain.repository.TransactionRepository;
+import com.api_payments.domain.repository.UsuarioLojistaRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class TransactionService {
     private final TransactionRepository transactionRepository;
+    private final UsuarioLojistaRepository usuarioLojistaRepository;
 
     @Transactional
     public ResponseEntity<Transaction> todasTrasaçoes(Long id){
         return transactionRepository.findById(id).map(ResponseEntity::ok).orElseThrow(()-> new TransactionException("sem transações esse id"));
+    }
+
+    @Transactional
+    public Transaction fazerTransaçao(Transaction transaction){
+        if (transaction.equals(transaction.getStatus().equals(StatusTransaction.FAILED))){
+          throw new TransactionException("Error na trasaçao faliu");
+        }
+        if (transaction.getValor().signum() <0){
+            throw new TransactionException("o valor tem que ser maior que zero");
+        }
+        transaction.setDataTransaction(OffsetDateTime.now());
+
+
+        return transactionRepository.save(transaction);
     }
 }
