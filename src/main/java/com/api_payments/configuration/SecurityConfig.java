9ceb3c4b362364acfa2be967ce56b176b21f1063
Natan Moura <mouranatan933@gmail.com>
2024-08-06 15:39:49 -0300

    fix: resolve bug data null

--- src/main/java/com/api_payments/configuration/SecurityConfig.java
+++ src/main/java/com/api_payments/configuration/SecurityConfig.java
@@ -2,20 +2,33 @@ package com.api_payments.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf(csrf->csrf.disable())
-                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorization -> authorization.
+                        requestMatchers(HttpMethod.POST, "/transaçoes").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/v1/api/usuarios").permitAll()
+                        .anyRequest().authenticated())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/api_payments/filter/JwtRequestFilter.java
+++ src/main/java/com/api_payments/filter/JwtRequestFilter.java
@@ -1,23 +1,23 @@
-package com.api_payments.filter;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-
-@Component
-public class JwtRequestFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        final String authorizationHeader = request.getHeader("Authorization");
-        String username = null;
-        String jwt = null;
-        if (authorizationHeader !=null && authorizationHeader.startsWith("Bearer")){
-          jwt = authorizationHeader.substring(7);
-        }
-    }
-}
+//package com.api_payments.filter;
+//
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import java.io.IOException;
+//
+//@Component
+//public class JwtRequestFilter extends OncePerRequestFilter {
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//        final String authorizationHeader = request.getHeader("Authorization");
+//        String username = null;
+//        String jwt = null;
+//        if (authorizationHeader !=null && authorizationHeader.startsWith("Bearer")){
+//          jwt = authorizationHeader.substring(7);
+//        }
+//    }
+//}
